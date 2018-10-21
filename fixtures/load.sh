9018ce42571e76f80db8de9368eb3cc6dcd82fe6
natoinet <antoine.brunel@gmail.com>
2018-10-21 17:07:53 +0200

    Loading fixtures is now more elegant here too

--- fixtures/load.sh
+++ fixtures/load.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
+APP_PATH=tucat/twitter_extraction/fixtures/
 
 python manage.py makemigrations twitter_extraction
 python manage.py migrate --fake-initial
 
-python manage.py loaddata application.json
+function importfixtures {
+  for fixture in "$@"
+  do
+    if [ -e $APP_PATH$fixture.json ]
+    then
+      echo "Importing $fixture"
+      python manage.py loaddata $APP_PATH$fixture.json
+    else
+      echo "No $fixture to import"
+    fi
+  done
+}
 
-python manage.py loaddata twitter_extraction.exportationformat.json
-python manage.py loaddata twitter_extraction.exportationtype.json
-python manage.py loaddata twitter_extraction.twitterlistextraction.json
+importfixtures application twitter_extraction.exportationformat twitter_extraction.exportationtype twitter_extraction.twitterlistextraction
